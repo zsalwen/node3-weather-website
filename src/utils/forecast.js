2d53ebf4d1c944be0eebbfc9ea17db463fca0e12
zsalwtop <zsalwen@wtop.com>
2019-05-13 16:07:20 -0400

    More detailed data in forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,9 +9,12 @@ const forecast = (longitude, latitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
+            const summary = body.daily.data[0].summary
+            const high = body.daily.data[0].temperatureHigh
+            const low = body.daily.data[0].temperatureLow
             const temperature = body.currently.temperature
             const precipProbability = body.currently.precipProbability
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + temperature + ' degrees out. There is a ' + precipProbability + '% chance of rain.')
+            callback(undefined, summary + ' The high today is ' + high + ', with a low of ' + low + '. It is currently ' + temperature + ' degrees out. There is a ' + precipProbability + '% chance of rain.')
         }
     })
 }
